d3ab89b106dd08421d724254c9e1bcf090c9ee3c
WebDevMullins <webdevmullins@gmail.com>
2024-01-30 19:00:15 -0600

    Add AIcons project to projects.js

--- src/lib/projects.js
+++ src/lib/projects.js
@@ -1,5 +1,11 @@
 export const projects = [
 	{
+		title: 'AIcons',
+		github: 'https://github.com/WebDevMullins/project-3',
+		href: 'https://project-3-8j08.onrender.com',
+		img: '/projects/aiconic.png'
+	},
+	{
 		title: 'Average Joes',
 		github: 'https://github.com/WebDevMullins/average-joes',
 		href: 'https://wdm-average-joes-88b9a3337bf4.herokuapp.com/',
@@ -28,11 +34,5 @@ export const projects = [
 		github: 'https://github.com/WebDevMullins/MVC-Tech-Blog',
 		href: 'https://wdm-techblog-14e8d68557e0.herokuapp.com/',
 		img: '/projects/techblog.png'
-	},
-	{
-		title: 'PWA - Text Editor',
-		github: 'https://github.com/WebDevMullins/pwa-text-editor',
-		href: 'https://github.com/WebDevMullins/pwa-text-editor',
-		img: '/projects/pwa.png'
 	}
 ]
