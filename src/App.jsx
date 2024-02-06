aa2a6f2a14c15635580a3db9cfa5a5463d1fe93c
WebDevMullins <webdevmullins@gmail.com>
2024-02-06 11:57:23 -0600

    Add @vercel/analytics package

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,5 @@
 import { NextUIProvider } from '@nextui-org/react'
+import { inject } from '@vercel/analytics/*'
 import { Outlet, useNavigate } from 'react-router-dom'
 
 import './App.css'
@@ -9,7 +10,8 @@ import Header from './components/Header'
 function App() {
 	// React Router hook for programmatic navigation
 	const navigate = useNavigate()
-
+	inject()
+	
 	return (
 		<>
 			{/* NextUIProvider to provide the NextUI theme and navigation context */}
